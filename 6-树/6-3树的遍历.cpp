9ca03d77a0fb30a0c4f5241bd9ec4ed12941c43c
jdhantianxu@163.com <jdhantianxu@163.com>
2025-03-25 21:36:13 +0800

    3月25学习打卡

--- 6-树/6-3树的遍历.cpp
+++ 6-树/6-3树的遍历.cpp
@@ -7,7 +7,137 @@ typedef struct TreeNode
 {
     E element;
     struct TreeNode *left, *right;
+    int flag;
 } *Node;
+
+typedef Node ElemType;
+
+// 栈的实现
+
+typedef struct ListNode {
+    ElemType element;
+    struct ListNode *next;
+}LNode , *List;
+
+typedef struct QNode
+{
+    ElemType element;
+    struct QNode *next;
+} QNode, *Queue;
+
+typedef struct LinkedQ
+{
+    Queue front, rear;
+} *LinkedQueue;
+
+Queue CreateQueue()
+{
+    Queue head = (Queue)malloc(sizeof(QNode));
+    head->next = NULL;
+    return head;
+}
+
+LinkedQueue initLinkedQueue()
+{
+    LinkedQueue lq = (LinkedQueue)malloc(sizeof(struct LinkedQ));
+    lq->front = lq->rear = NULL;
+    return lq;
+}
+
+bool is_Empty(LinkedQueue lq)
+{
+    return lq->front == NULL && lq->rear == NULL;
+}
+
+bool EnQueue(LinkedQueue lq, ElemType e)
+{
+    Queue node = CreateQueue();
+    if (node == NULL)
+        return false;
+    node->element = e;
+    if (is_Empty(lq))
+        lq->front = lq->rear = node;
+    else
+    {
+        node->next = lq->rear->next;
+        lq->rear->next = node;
+        lq->rear = lq->rear->next;
+    }
+    return true;
+}
+
+ElemType DeQueue(LinkedQueue lq)
+{
+    if (is_Empty(lq))
+        return NULL;
+    ElemType elem = lq->front->element;
+    if (lq->front == lq->rear)
+        lq->front = lq->rear = NULL;
+    else
+        lq->front = lq->front->next;
+    return elem;
+}
+
+void PrintQueue(LinkedQueue lq)
+{
+    Queue q = lq->front;
+    while (q)
+    {
+        printf("%d ", q->element);
+        q = q->next;
+    }
+    printf("\n");
+}
+
+List CreateStack()
+{
+    List head = (List)malloc(sizeof(Node));
+    head->next = NULL;
+    return head;
+}
+
+bool Push(List head,ElemType e)
+{
+    List node = CreateStack();
+    if(node == NULL) return false;
+    node->element = e;
+    node->next = head->next;
+    head->next = node;
+    return true;
+}
+
+ElemType getTop(List head)
+{
+    return head->next->element;
+}
+
+ElemType Pop(List head)
+{
+    if(head->next==NULL) return NULL;
+    ElemType elem = head->next->element;
+    List p = head->next;
+    head->next = p->next;
+    free(p);
+    return elem;
+}
+
+bool is_Empty(List head)
+{
+    return head->next == NULL;
+}
+
+void PrintStack(List head)
+{
+    head = head->next;
+    while(head)
+    {
+        printf("%c ",head->element->element);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+// END
 
 void preOrder(TreeNode *tree)
 {
@@ -17,6 +147,92 @@ void preOrder(TreeNode *tree)
     preOrder(tree->left);
     preOrder(tree->right);
 }
+
+void preOrder0(TreeNode *tree)
+{
+    List stack = CreateStack();
+    while(tree || !is_Empty(stack))
+    {
+        while(tree)
+        {
+            cout<<tree->element<<" ";
+            Push(stack,tree);
+            tree = tree->left;
+            PrintStack(stack);
+        }
+        do {tree = Pop(stack);} while(!tree->right);
+        tree = tree->right;
+    }
+}
+
+void inOrder(TreeNode *root)
+{
+    if(root==NULL) return;
+    inOrder(root->left);
+    cout<<root->element<<" ";
+    inOrder(root->right);
+}
+
+void inOrder0(TreeNode *root)
+{
+    List stack = CreateStack();
+    while(root || !is_Empty(stack))
+    {
+        while(root)
+        {
+            Push(stack,root);
+            root->flag = 0; // 0 表示左子树遍历完成
+            root = root->left;
+        }
+        root = Pop(stack);
+        cout<<root->element<<" ";
+        root = root->right; // 如果没有右节点，正好就是返回上一个
+    }
+}
+
+void postOrder(TreeNode *root)
+{
+    if(root==NULL) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->element<<" ";
+}
+
+void postOrder0(TreeNode *root)
+{
+    List stack = CreateStack();
+    while(root || !is_Empty(stack))
+    {
+        while(root)
+        {
+            Push(stack,root);
+            root->flag = 0;
+            root = root->left;
+        }
+        root = getTop(stack);
+        if(root->flag==0) {
+            root->flag = 1;
+            root = root->right;
+        } else {
+            cout<<root->element<<" ";
+            Pop(stack);
+            root = NULL; // 跳过while，继续取栈里面的结点
+        }
+    }
+}
+
+void levelOrder(TreeNode *tree)
+{
+    LinkedQueue q = initLinkedQueue();
+    while(tree || !is_Empty(q))
+    {
+        cout<<tree->element<<" ";
+        if(tree->left) EnQueue(q,tree->left); // if不能少
+        if(tree->right) EnQueue(q,tree->right);
+        tree = DeQueue(q);
+    }
+}
+
 
 int main()
 {
@@ -43,7 +259,10 @@ int main()
     e->left = e->right = NULL;
     f->left = f->right = NULL;
 
-    preOrder(a);
+    // preOrder0(a);
+    // inOrder0(a);
+    // postOrder0(a);
+    levelOrder(a);
 
     return 0;
 }
