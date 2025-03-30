3e9c92405060742341cd245a6fd27e98f3288984
jdhantianxu@163.com <jdhantianxu@163.com>
2025-03-30 13:26:33 +0800

    3.29

--- 6-树/6-7红黑树.cpp
+++ 6-树/6-7红黑树.cpp
@@ -1,1 +1,453 @@
-// 面试重点，先搞清原理，代码先放过
+#include <iostream>
+using namespace std;
+
+// claude编写
+
+enum Color {
+    red, black
+};
+
+class Node 
+{
+public:
+    int data;
+    Node *left, *right, *parent;
+    Color color;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->color = red;
+        this->left = this->right = this->parent = nullptr;
+    }
+};
+
+class RedBlackTree
+{
+private:
+    Node *root;
+    
+    // Utility functions
+    void leftRotate(Node *x);
+    void rightRotate(Node *y);
+    void fixViolation(Node *&root, Node *&pt);
+    void inorderHelper(Node *root);
+    void fixDeleteViolation(Node *x, Node *parent, bool isLeft);
+    Node* BST_insert(Node *root, Node *pt);
+    Node* minimum(Node *node);
+    void rbTransplant(Node *u, Node *v);
+    void deleteFixup(Node *x, Node *xParent, bool isLeft);
+    
+public:
+    RedBlackTree() { root = nullptr; }
+    
+    void insert(int data);
+    void remove(int data);
+    void inorder();
+    Node* search(int data);
+};
+
+// Left rotation
+void RedBlackTree::leftRotate(Node *x)
+{
+    Node *y = x->right;
+    x->right = y->left;
+    
+    if (y->left != nullptr)
+        y->left->parent = x;
+    
+    y->parent = x->parent;
+    
+    if (x->parent == nullptr)
+        root = y;
+    else if (x == x->parent->left)
+        x->parent->left = y;
+    else
+        x->parent->right = y;
+    
+    y->left = x;
+    x->parent = y;
+}
+
+// Right rotation
+void RedBlackTree::rightRotate(Node *y)
+{
+    Node *x = y->left;
+    y->left = x->right;
+    
+    if (x->right != nullptr)
+        x->right->parent = y;
+    
+    x->parent = y->parent;
+    
+    if (y->parent == nullptr)
+        root = x;
+    else if (y == y->parent->left)
+        y->parent->left = x;
+    else
+        y->parent->right = x;
+    
+    x->right = y;
+    y->parent = x;
+}
+
+// Fix violations after insertion
+void RedBlackTree::fixViolation(Node *&root, Node *&pt)
+{
+    Node *parent_pt = nullptr;
+    Node *grand_parent_pt = nullptr;
+    
+    while ((pt != root) && (pt->color != black) && (pt->parent->color == red))
+    {
+        parent_pt = pt->parent;
+        grand_parent_pt = pt->parent->parent;
+        
+        // Case A: Parent of pt is left child of Grand-parent of pt
+        if (parent_pt == grand_parent_pt->left)
+        {
+            Node *uncle_pt = grand_parent_pt->right;
+            
+            // Case 1: The uncle of pt is also red
+            if (uncle_pt != nullptr && uncle_pt->color == red)
+            {
+                grand_parent_pt->color = red;
+                parent_pt->color = black;
+                uncle_pt->color = black;
+                pt = grand_parent_pt;
+            }
+            else
+            {
+                // Case 2: pt is right child of its parent
+                if (pt == parent_pt->right)
+                {
+                    leftRotate(parent_pt);
+                    pt = parent_pt;
+                    parent_pt = pt->parent;
+                }
+                
+                // Case 3: pt is left child of its parent
+                rightRotate(grand_parent_pt);
+                swap(parent_pt->color, grand_parent_pt->color);
+                pt = parent_pt;
+            }
+        }
+        // Case B: Parent of pt is right child of Grand-parent of pt
+        else
+        {
+            Node *uncle_pt = grand_parent_pt->left;
+            
+            // Case 1: The uncle of pt is also red
+            if (uncle_pt != nullptr && uncle_pt->color == red)
+            {
+                grand_parent_pt->color = red;
+                parent_pt->color = black;
+                uncle_pt->color = black;
+                pt = grand_parent_pt;
+            }
+            else
+            {
+                // Case 2: pt is left child of its parent
+                if (pt == parent_pt->left)
+                {
+                    rightRotate(parent_pt);
+                    pt = parent_pt;
+                    parent_pt = pt->parent;
+                }
+                
+                // Case 3: pt is right child of its parent
+                leftRotate(grand_parent_pt);
+                swap(parent_pt->color, grand_parent_pt->color);
+                pt = parent_pt;
+            }
+        }
+    }
+    
+    root->color = black;
+}
+
+// Standard BST insertion
+Node* RedBlackTree::BST_insert(Node *root, Node *pt)
+{
+    if (root == nullptr)
+        return pt;
+    
+    if (pt->data < root->data)
+    {
+        root->left = BST_insert(root->left, pt);
+        root->left->parent = root;
+    }
+    else if (pt->data > root->data)
+    {
+        root->right = BST_insert(root->right, pt);
+        root->right->parent = root;
+    }
+    
+    return root;
+}
+
+// Insert a new key
+void RedBlackTree::insert(int data)
+{
+    Node *pt = new Node(data);
+    
+    // Do a normal BST insert
+    root = BST_insert(root, pt);
+    
+    // Fix Red-Black Tree violations
+    fixViolation(root, pt);
+}
+
+// Find the node with minimum value
+Node* RedBlackTree::minimum(Node *node)
+{
+    while (node->left != nullptr)
+        node = node->left;
+    
+    return node;
+}
+
+// Replace u with v
+void RedBlackTree::rbTransplant(Node *u, Node *v)
+{
+    if (u->parent == nullptr)
+        root = v;
+    else if (u == u->parent->left)
+        u->parent->left = v;
+    else
+        u->parent->right = v;
+    
+    if (v != nullptr)
+        v->parent = u->parent;
+}
+
+void RedBlackTree::fixDeleteViolation(Node *x, Node *parent, bool isLeft)
+{
+    while (x != root && (x == nullptr || x->color == black))
+    {
+        if (isLeft)
+        {
+            Node *w = parent->right;
+            
+            if (w->color == red)
+            {
+                w->color = black;
+                parent->color = red;
+                leftRotate(parent);
+                w = parent->right;
+            }
+            
+            if ((w->left == nullptr || w->left->color == black) && 
+                (w->right == nullptr || w->right->color == black))
+            {
+                w->color = red;
+                x = parent;
+                if (x->parent != nullptr)
+                {
+                    isLeft = (x == x->parent->left);
+                    parent = x->parent;
+                }
+                else
+                    break;
+            }
+            else
+            {
+                if (w->right == nullptr || w->right->color == black)
+                {
+                    if (w->left != nullptr)
+                        w->left->color = black;
+                    w->color = red;
+                    rightRotate(w);
+                    w = parent->right;
+                }
+                
+                w->color = parent->color;
+                parent->color = black;
+                if (w->right != nullptr)
+                    w->right->color = black;
+                leftRotate(parent);
+                x = root; // To break the loop
+            }
+        }
+        else
+        {
+            Node *w = parent->left;
+            
+            if (w->color == red)
+            {
+                w->color = black;
+                parent->color = red;
+                rightRotate(parent);
+                w = parent->left;
+            }
+            
+            if ((w->right == nullptr || w->right->color == black) && 
+                (w->left == nullptr || w->left->color == black))
+            {
+                w->color = red;
+                x = parent;
+                if (x->parent != nullptr)
+                {
+                    isLeft = (x == x->parent->left);
+                    parent = x->parent;
+                }
+                else
+                    break;
+            }
+            else
+            {
+                if (w->left == nullptr || w->left->color == black)
+                {
+                    if (w->right != nullptr)
+                        w->right->color = black;
+                    w->color = red;
+                    leftRotate(w);
+                    w = parent->left;
+                }
+                
+                w->color = parent->color;
+                parent->color = black;
+                if (w->left != nullptr)
+                    w->left->color = black;
+                rightRotate(parent);
+                x = root; // To break the loop
+            }
+        }
+    }
+    
+    if (x != nullptr)
+        x->color = black;
+}
+
+// Remove a node with given data
+void RedBlackTree::remove(int data)
+{
+    if (root == nullptr)
+        return;
+    
+    Node *z = search(data);
+    
+    if (z == nullptr)
+    {
+        cout << "No node found to delete with value: " << data << endl;
+        return;
+    }
+    
+    Node *y = z;
+    Node *x = nullptr;
+    Node *xParent = nullptr;
+    bool isLeft = false;
+    Color yOriginalColor = y->color;
+    
+    if (z->left == nullptr)
+    {
+        x = z->right;
+        xParent = z->parent;
+        isLeft = (z->parent != nullptr && z == z->parent->left);
+        rbTransplant(z, z->right);
+    }
+    else if (z->right == nullptr)
+    {
+        x = z->left;
+        xParent = z->parent;
+        isLeft = (z->parent != nullptr && z == z->parent->left);
+        rbTransplant(z, z->left);
+    }
+    else
+    {
+        y = minimum(z->right);
+        yOriginalColor = y->color;
+        x = y->right;
+        
+        if (y->parent == z)
+        {
+            if (x != nullptr)
+                x->parent = y;
+            xParent = y;
+            isLeft = false;
+        }
+        else
+        {
+            xParent = y->parent;
+            isLeft = (y == y->parent->left);
+            rbTransplant(y, y->right);
+            y->right = z->right;
+            if (y->right != nullptr)
+                y->right->parent = y;
+        }
+        
+        rbTransplant(z, y);
+        y->left = z->left;
+        y->left->parent = y;
+        y->color = z->color;
+    }
+    
+    delete z;
+    
+    if (yOriginalColor == black)
+    {
+        fixDeleteViolation(x, xParent, isLeft);
+    }
+}
+
+// Search for a node with given data
+Node* RedBlackTree::search(int data)
+{
+    Node *temp = root;
+    
+    while (temp != nullptr)
+    {
+        if (data < temp->data)
+            temp = temp->left;
+        else if (data > temp->data)
+            temp = temp->right;
+        else
+            return temp;
+    }
+    
+    return nullptr;
+}
+
+// Inorder traversal
+void RedBlackTree::inorderHelper(Node *root)
+{
+    if (root == nullptr)
+        return;
+    
+    inorderHelper(root->left);
+    cout << root->data << "(" << (root->color == red ? "Red" : "Black") << ") ";
+    inorderHelper(root->right);
+}
+
+void RedBlackTree::inorder()
+{
+    inorderHelper(root);
+    cout << endl;
+}
+
+int main()
+{
+    RedBlackTree tree;
+    
+    // Example usage
+    tree.insert(7);
+    tree.insert(3);
+    tree.insert(18);
+    tree.insert(10);
+    tree.insert(22);
+    tree.insert(8);
+    tree.insert(11);
+    tree.insert(26);
+    
+    cout << "Inorder traversal of the red-black tree: " << endl;
+    tree.inorder();
+    
+    cout << "Removing 18, 11, 3" << endl;
+    tree.remove(18);
+    tree.remove(11);
+    tree.remove(3);
+    
+    cout << "Inorder traversal after deletion: " << endl;
+    tree.inorder();
+    
+    return 0;
+}
---
+++ 6-树/6-9哈夫曼树.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+#define T Tree
+
+#define E char
+
+// 哈夫曼树
+typedef struct TreeNode {
+    E element;
+    TreeNode *left;
+    TreeNode *right;
+    int value; // 权值
+    string code;
+} *Tree;
+
+// 优先级队列
+typedef struct LNode {
+    T element;
+    LNode *next;
+} *List;
+
+struct Queue
+{
+    List front,rear;
+};
+
+typedef struct Queue *LinkedQueue;
+
+bool initQueue(LinkedQueue queue)
+{
+    List node = new LNode;
+    if(node == NULL) return 0;
+    queue->front = queue->rear = node;
+    node->next = NULL; // 因为会用到判断下一个是否为空
+    return true;
+}
+
+// 优先级插入方式
+bool offerQueue(LinkedQueue queue,T element)
+{
+    List node = new LNode;
+    if(node == NULL) return false;
+    node->element = element;
+    node->next = NULL;
+    List pre = queue->front;
+    while(pre->next && pre->next->element->value <= element->value)
+        // 从小到大的优先级队列，这里寻找pre->element比element大的pre
+        pre = pre->next;
+    if(pre == queue->rear) {
+        // 插入队尾
+        queue->rear->next = node;
+        queue->rear = node;
+    } else {
+        // VIP模式，插在pre后面
+        node->next = pre->next;
+        pre->next = node;
+    }
+    return true;
+}
+
+T pollQueue(LinkedQueue queue)
+{
+    T elem = queue->front->next->element; // 跳过首节点，取第一个
+    List node = queue->front->next; 
+    queue->front->next = queue->front->next->next; 
+    if(queue->rear == node) queue->rear = queue->front;
+    delete node;
+    return elem;
+}
+
+// 会从小到大输出
+void printQueue(LinkedQueue queue)
+{
+    cout<<"<<< ";
+    List node = queue->front->next; // 跳过首节点，取第一个
+    while(node)
+    {
+        cout<<node->element->element<<" ";
+        node = node->next;
+    }
+    cout<<"<<<"<<endl;
+}
+
+// 创建二叉树结点
+Tree createNode(E element, int value)
+{
+    Tree node = new TreeNode;
+    node->element = element;
+    node->left = node->right = NULL;
+    node->value = value;
+    return node;
+}
+
+void inOrder(Tree root)
+{
+    if(root==NULL) return;
+    inOrder(root->left);
+    // cout<<root->element<<" ";
+    if(root->element!='r') cout<<root->code<<endl;
+    inOrder(root->right);
+}
+
+// 写的没看懂
+// char* encode(Tree root,E elem)
+// {
+//     if(root == NULL) return NULL;
+//     if(root->element == elem) return "";
+//     char *str = encode(root->left,elem);
+//     char *s = (char*)malloc(sizeof(char)*10);
+//     if(str != NULL)
+//     {
+//         s[0] = '0';
+//         str = strcat(s,str);
+//     } else {
+//         str = encode(root->right,elem);
+//         if(str != NULL)
+//         {
+//             s[0] = '1';
+//             str = strcat(s,str);
+//         }
+//     }
+//     return str;
+// }
+
+// 最佳编码方案
+void Encode(Tree root,string str)
+{
+    if(root == NULL) return ;
+    root->code = str;
+    Encode(root->left,str+"0");
+    Encode(root->right,str+"1");
+}
+
+string encode(Tree root, string str, E elem) {
+    if (root == nullptr) return "";
+    if (root->element == elem) return str;
+    // 先探索左子树，若找到则直接返回编码
+    string left = encode(root->left, str + "0", elem);
+    if (!left.empty()) return left;
+    // 左子树未找到，再探索右子树并返回结果
+    string right = encode(root->right, str + "1", elem);
+    return right;
+}
+
+
+
+int main()
+{
+    LinkedQueue queue = new Queue;
+    initQueue(queue);
+    
+    offerQueue(queue,createNode('A',5));
+    offerQueue(queue,createNode('B',16));
+    offerQueue(queue,createNode('C',8));
+    offerQueue(queue,createNode('D',13));
+    // printQueue(queue); // A C D B
+
+    
+    while(queue->front->next != queue->rear)
+    {
+        Tree left = pollQueue(queue);
+        Tree right = pollQueue(queue);
+        Tree node = createNode('r',left->value + right->value);
+        node->left = left;
+        node->right = right;
+        offerQueue(queue,node);  
+    }
+
+    Tree root = pollQueue(queue);
+    Encode(root,"");
+    // inOrder(root);
+    cout<<encode(root,"",'B')<<endl;
+    cout<<encode(root,"",'D')<<endl;
+    cout<<encode(root,"",'A')<<endl;
+    cout<<encode(root,"",'C')<<endl;
+
+    return 0;
+}
